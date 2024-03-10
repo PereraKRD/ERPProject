40dbd87bb5d4b53d2756f9df468dccff5dd42260
lasith54 <lasithperera254@gmail.com>
2024-03-10 17:28:29 +0530

    Student Result List and Request Page Added

---
+++ Application/ERP.Application.AcademicApp/Modules/Interfaces/IGetResultListByStudentResultUseCase.cs
@@ -0,0 +1,14 @@
+﻿using ERP.Domain.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Application.AcademicApp.Modules.Interfaces
+{
+    public interface IGetResultListByStudentResultUseCase
+    {
+        Task<List<StudentResult>> ExecuteAsync(int evaluationId);
+    }
+}
